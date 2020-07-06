ecb908d447f811ddbe98a1fd0ad65d7ccdeaaded
Anastasia <roshepkina34@gmail.com>
2020-07-06 20:21:19 +0400

    Task 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,16 @@ public class App {
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println (bean.getMessage ());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean ("helloworld");
+        System.out.println(bean2.getMessage());
+        System.out.println (bean.equals (bean2));
+        Cat catBean  = (Cat) applicationContext.getBean ("cat");
+        System.out.println (catBean.getMessage ());
+        Cat catBean2  = (Cat) applicationContext.getBean ("cat");
+        System.out.println (catBean2.getMessage ());
+        System.out.println (catBean.equals (catBean2));
+
+
     }
 }
